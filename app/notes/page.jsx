7048c4f1bb65a089b34ce25c9be34378d0a39548
inspizzz <wiktor.wiejak@gmail.com>
2024-04-07 18:54:51 +0100

    started developing the notes page with the top bar, line numbers and middle section

--- app/notes/page.jsx
+++ app/notes/page.jsx
@@ -1,7 +1,26 @@
 export default function NotePage() {
 	return (
-		<div>
-			<h1>Notes</h1>
+		<div className="w-full h-full flex flex-col gap-4 justify-between">
+			<div className="w-full h-16 bg-notes_background rounded-2xl p-2">
+
+			</div>
+
+			<div className="w-full h-full bg-notes_background rounded-2xl flex">
+				
+				{/* the numbers */}
+				<div className="w-8 h-full bg-gray-300 rounded-s-2xl flex flex-col p-1">
+					<p className="self-center">1</p>
+					<p className="self-center">2</p>
+					<p className="self-center">3</p>
+					<p className="self-center">.</p>
+					<p className="self-center">.</p>
+					<p className="self-center">.</p>
+				</div>
+
+				<div className="w-full h-full p-2 flex flex-col">
+					
+				</div>
+			</div>
 		</div>
 	)
 }
