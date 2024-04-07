57d4cf3e91ce059782bb848d34ed45a20a002eb3
inspizzz <wiktor.wiejak@gmail.com>
2024-04-07 15:57:49 +0100

    updated the top bar a little bit, made it more realistic

--- components/User/TopBar/index.jsx
+++ components/User/TopBar/index.jsx
@@ -2,35 +2,58 @@
 
 import { useUser } from "@/contexts/userContext"
 import {useState, useEffect} from 'react'
+
 import { MdLogout } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdHome } from "react-icons/md";
+
+import Link from "next/link";
+
+
 
 
 export function TopBar() {
 
 	const [ mounted, setMounted ] = useState(false)
+	const [ selected, setSelected ] = useState(false)
+
 	const { user, logout } = useUser()
 
 	useEffect(() => {
 		setMounted(true)
 	}, [])
 
-	return user && (
-
-		<div className="w-full h-10 absolute top-0 left-0 right-0 flex justify-center">
-			<div className="w-full flex justify-between items-center bg-black p-2">
-				<div className="">
-
-				</div>
-
-				<div>
+	return (mounted && user) && (
+		
+		<div className="absolute top-0 right-0 flex flex-col gap-2 justify-end p-2">
+			<div className="w-full flex justify-end">
+				<div className="w-12 h-12 rounded-full flex justify-center bg-notes_background hover:shadow cursor-pointer" onClick={() => {
+					setSelected(!selected)
+				}}>
+					{
+						!selected ? (
+							<p className="self-center">WW</p>
+						) : (
+							<MdLogout className="self-center text-black" />
+						)
+					}
 					
 				</div>
-
-				<div className="">
-					<a className="text-white px-4 py-2 rounded" onClick={() => logout()}><MdLogout /></a>
-				</div>
-				
 			</div>
+			
+			{
+				selected && (
+					<div className="bg-notes_background w-full rounded-2xl flex flex-col gap-1 p-1">
+						<Link href={"/"} className="aspect-square rounded-2xl bg-orange-100 hover:bg-orange-300 flex justify-center"><MdHome className="self-center text-black" /></Link>
+						<Link  href={"/profile"} className="aspect-square rounded-2xl bg-orange-100 hover:bg-orange-300 flex justify-center"><CgProfile className="self-center text-black" /></Link>
+						<button className="aspect-square rounded-2xl bg-orange-100 hover:bg-orange-300 flex justify-center"></button>
+						<Link href={"/settings"} className="aspect-square rounded-2xl bg-orange-100 hover:bg-orange-300 flex justify-center"><IoSettingsOutline className="self-center text-black" /></Link>
+						<button className="aspect-square rounded-2xl bg-orange-100 hover:bg-orange-300 flex justify-center" onClick={logout}><MdLogout className="self-center text-black" /></button>
+					</div>
+				)
+			}
+			
 		</div>
 	) 
 }
